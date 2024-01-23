ff246336cb4a31dc0a63a9351a1bca3535561df2
Jakub Sabacinski <jakub.sabacinski@gmail.com>
2024-01-23 21:50:09 +0100

    added methods for Scoreboard

---
+++ Library/Domain/Scoreboard.cs
@@ -0,0 +1,24 @@
+﻿namespace Library.Domain;
+
+public class Scoreboard
+{
+    public Game? StartNewGame(string homeTeam, string awayTeam)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateScore(Guid gameId, int homeScore, int awayScore)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FinishGame(Guid gameId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Game> GetGamesSummary()
+    {
+        throw new NotImplementedException();
+    }
+}
